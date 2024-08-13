ef9d802d9c7924ff30cc1724e151aab4d221fdcd
Dheeraj <xettridheeraj01@gmail.com>
2024-08-13 22:01:08 +0545

    Added player movements
    used WASD to move player

--- Dooms_army/main.cpp
+++ Dooms_army/main.cpp
@@ -4,13 +4,23 @@
 int main() {
 	//------INITIALIZE-----
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Dooms army");
-	sf::CircleShape triangle(50.0f, 3);
-	triangle.setFillColor(sf::Color::Red);
-	triangle.setPosition(sf::Vector2f(100, 100));
-	//triangle.setOrigin(25.0f);
+	//------LOAD
+	sf::Texture playertexture;
+	sf::Sprite playersprite;
+	if (playertexture.loadFromFile("Assets/player/texture/playerright.png")) {
+		std::cout << "Player image loaded" << std::endl;
+		playersprite.setTexture(playertexture);
+		playersprite.setTextureRect(sf::IntRect(0,0,95, 155));
+		playersprite.setScale(sf::Vector2f(0.75, 0.75));
+	}
+	else {
+		std::cout << "Player image not loaded" << std::endl;
 
+	}
 	//------GAME LOOP
 	while (window.isOpen()) {
+			int Xindex =0, Yindex=0;//Initially the sprite will be setted to no booster one every other time 
+			playersprite.setTextureRect(sf::IntRect(Xindex * 95, Yindex * 155, 95, 155));
 
 		//------UPDATE
 		sf::Event event;
@@ -21,12 +31,32 @@ int main() {
 
 
 		}
-
+			sf::Vector2f position = playersprite.getPosition();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+			playersprite.setPosition(position + sf::Vector2f(0.75, 0));
+			playersprite.setScale(0.75, 0.75);
+			
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+			Xindex = 0;//this makes the booster appear only when flying up
+			Yindex = 1;
+			playersprite.setPosition(position + sf::Vector2f(0, -0.5));
+			playersprite.setTextureRect(sf::IntRect(Xindex * 95, Yindex * 155, 95, 155));
+			
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+			playersprite.setPosition(position + sf::Vector2f(-0.75, 0));
+			playersprite.setScale(-0.75,0.75);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+			playersprite.setPosition(position + sf::Vector2f(0, 0.75));
+		}
+		//------UPDATE
 		window.clear(sf::Color::Black);
 		//------DRAW
-		window.draw(triangle);
+		window.draw(playersprite);
 
 		window.display();
 	}
 	return 0;
-}
+} 
