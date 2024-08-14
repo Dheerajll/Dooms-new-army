940531c5a1eff3c0d86c824aedc55e86019d83c3
Dheeraj <xettridheeraj01@gmail.com>
2024-08-14 23:09:14 +0545

    Added a shooting mechanism

--- Dooms_army/main.cpp
+++ Dooms_army/main.cpp
@@ -1,28 +1,28 @@
 #include<iostream>
 #include<SFML/Graphics.hpp>
 
+
+
 int main() {
-	//------INITIALIZE-----
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Dooms army");
-	//------LOAD
+	//----------Initialize-------
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 10;
+	sf::RenderWindow window(sf::VideoMode(800, 600),"Dooms army",sf::Style::Default,settings);
 	sf::Texture playertexture;
 	sf::Sprite playersprite;
 	if (playertexture.loadFromFile("Assets/player/texture/playerright.png")) {
-		std::cout << "Player image loaded" << std::endl;
+		std::cout << "Player loaded" << std::endl;
 		playersprite.setTexture(playertexture);
-		playersprite.setTextureRect(sf::IntRect(0,0,95, 155));
-		playersprite.setScale(sf::Vector2f(0.75, 0.75));
-	}
-	else {
-		std::cout << "Player image not loaded" << std::endl;
+		playersprite.setScale(sf::Vector2f(0.75,0.75));
+		playersprite.setTextureRect(sf::IntRect(0, 0, 95, 155));
+		playersprite.setOrigin(sf::Vector2f(95 / 2, 155 / 2));
+		playersprite.setPosition(sf::Vector2f(400, 300));
 
 	}
-	//------GAME LOOP
+	std::vector<sf::RectangleShape>bullets;
+	float bulletspeed = 0.005;
+	//----------Initialize-----
 	while (window.isOpen()) {
-			int Xindex =0, Yindex=0;//Initially the sprite will be setted to no booster one every other time 
-			playersprite.setTextureRect(sf::IntRect(Xindex * 95, Yindex * 155, 95, 155));
-
-		//------UPDATE
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
@@ -31,32 +31,47 @@ int main() {
 
 
 		}
+		//-------UPDATE-------
 			sf::Vector2f position = playersprite.getPosition();
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-			playersprite.setPosition(position + sf::Vector2f(0.75, 0));
-			playersprite.setScale(0.75, 0.75);
-			
-		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-			Xindex = 0;//this makes the booster appear only when flying up
-			Yindex = 1;
-			playersprite.setPosition(position + sf::Vector2f(0, -0.5));
-			playersprite.setTextureRect(sf::IntRect(Xindex * 95, Yindex * 155, 95, 155));
-			
+			playersprite.setPosition(position + sf::Vector2f(0,-1));
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-			playersprite.setPosition(position + sf::Vector2f(-0.75, 0));
-			playersprite.setScale(-0.75,0.75);
+			playersprite.setPosition(position + sf::Vector2f(-1, 0));
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-			playersprite.setPosition(position + sf::Vector2f(0, 0.75));
+			playersprite.setPosition(position + sf::Vector2f(0, 1));
 		}
-		//------UPDATE
-		window.clear(sf::Color::Black);
-		//------DRAW
-		window.draw(playersprite);
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+			playersprite.setPosition(position + sf::Vector2f(1, 0));
+		}
 
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+			bullets.push_back(sf::RectangleShape(sf::Vector2f(50,25)));
+			int i = bullets.size() - 1;
+			bullets[i].setPosition(playersprite.getPosition());
+		}
+		for (size_t i = 0; i < bullets.size(); i++)
+		{
+			sf::Vector2f bulletdirection;
+			bulletdirection = sf::Vector2f(800, 600) - bullets[i].getPosition();
+			bullets[i].setPosition(bullets[i].getPosition()+bulletdirection*bulletspeed);
+		}
+		//-------UPDATE-------
+		// 
+		//-------DRAW
+		window.clear(sf::Color::Black);
+
+		window.draw(playersprite);
+		for (size_t i = 0; i < bullets.size(); i++)
+		{
+			window.draw(bullets[i]);
+		}
 		window.display();
+		//-------DRAW
 	}
+
+
+
 	return 0;
-} 
+}
